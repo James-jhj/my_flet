7a5e5e3e6883d5776d5a5cdf5a38c6993dc477a0
jianghuajie1983@163.com <jianghuajie1983@163.com>
2026-05-02 11:39:05 +0800

    更新main.py：文字居中显示

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import flet as ft
 
 def main(page: ft.Page):
-    page.title = "Hello from Flet!"
-    page.add(ft.Text("Hello, 华为手机!"))
+    page.title = "Design By James!"
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER  # 水平居中
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER      # 垂直居中
+    
+    page.add(
+        ft.Text("Hello, 蒋华杰，你好呀，这是你的第一个Android程序哟，很棒!")
+    )
 
 ft.app(target=main)
