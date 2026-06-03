e08345dbd8ca83651fc565bd5d8b0dd34731d42c
jianghuajie1983@163.com <jianghuajie1983@163.com>
2026-06-03 10:38:13 +0800

    release: v1.0.16

--- main.py
+++ main.py
@@ -51,8 +51,8 @@ else:
         print("警告: pyncm 模块不可用")
 
 # ========== 版本信息 ==========
-APP_VERSION = "1.0.15"
-APP_VERSION_CODE = 15
+APP_VERSION = "1.0.16"
+APP_VERSION_CODE = 16
 # =============================
 
 class AnalogClock(ft.Container):
